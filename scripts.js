020e13c311db50ad0d9930490833eba4d69808ca
Rehab Mansi <120115164+rehabmansi811@users.noreply.github.com>
2024-09-24 00:34:51 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// Back to Top Button
+const backToTopBtn = document.getElementById("backToTop");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+};
+
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
